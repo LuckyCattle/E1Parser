8b7722bf0280ee3499e6b80dbf074a3154bb682d
sugarbaron <sugarbaron1@mail.ru>
2016-11-20 18:56:07 +0600

    claned some trash

--- E1ParserTests/WebPageLoaderModuleTests.cs
+++ E1ParserTests/WebPageLoaderModuleTests.cs
@@ -10,13 +10,6 @@ namespace E1Parser.Tests {
             WebPageLoader loader = new WebPageLoaderModule();
 
             string e1PageCode = loader.GetPageCode("http://www.e1.ru/afisha/events/art");
-            
-            //FIXME:debug//////////////////////////////////////////////////////
-            /*FileStream filewriter = new FileStream("./debug.log", FileMode.Create);
-            StreamWriter writer = new StreamWriter(filewriter);
-            writer.Write(e1PageCode);
-            writer.Close();*/
-            ///////////////////////////////////////////////////////////////////
 
             bool isCorrect = e1PageCode.Contains("favicon.ico");
             Assert.IsTrue(isCorrect);
