f56d7bf0546a74787611b03343595faef1d88603
sugarbaron <sugarbaron1@mail.ru>
2016-11-21 15:20:12 +0600

    written code for ModelModule

---
+++ E1Parser/ModelModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace E1Parser {
+    public sealed class ModelModule : Model {
+        private WebPageLoader pageLoader;
+        private Parser e1Parser;
+        ArrayList e1Events;
+        ArrayList observers;
+
+        public ModelModule() {
+            pageLoader = new WebPageLoaderModule();
+            e1Parser  = new ParserModule(pageLoader);
+            observers = new ArrayList();
+        }
+
+        public void RegisterObserver(View view) {
+            observers.Add(view);
+        }
+
+        public void RefreshContent() {
+            e1Events = e1Parser.ExtractEvents();
+            notifyObservers();
+        }
+
+        private void notifyObservers() {
+            foreach (View eachObserver in observers) {
+                eachObserver.Update(e1Events);
+            }
+        }
+    }
+}
